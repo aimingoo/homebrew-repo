eff461ec10a124533920489749f14f66c4a556e7
aimingoo <aiming@gmail.com>
2017-09-17 23:48:56 -0500

    Update monster.rb

--- monster.rb
+++ monster.rb
@@ -3,7 +3,7 @@ class Monster < Formula
   homepage "https://github.com/aimingoo/monster"
 
   url "https://github.com/aimingoo/monster/archive/v1.0.0.zip"
-  mirror "https://aimingoo.github.io/releases/monster/monster-1.0.0.zip"
+  mirror "https://aimingoo.github.io/content/releases/monster/monster-1.0.0.zip"
   version "1.0.0"
   sha256 "d8e12958c388ca6078579202db675052a8976a4f1ce1f446b4a5b17b909f0e5d"
 
