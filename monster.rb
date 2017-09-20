182244c1212474dd4c6350694c8515507e711ba4
aimingoo <aimingoo@nanchao.org>
2017-09-20 11:52:14 +0800

    monster v1.0.3 released

--- monster.rb
+++ monster.rb
@@ -1,11 +1,11 @@
 class Monster < Formula
   desc "Full or incremental static site generator for Ghost"
   homepage "https://github.com/aimingoo/monster"
-  version "1.0.2"
+  version "1.0.3"
 
   url "https://github.com/aimingoo/monster/archive/v#{version}.tar.gz"
   mirror "https://aimingoo.github.io/content/releases/monster/monster-#{version}.tar.gz"
-  sha256 "2265680d25fed4c1ace3b99febb87f77ab3ce3d6f21bd889f481e3a4847add79"
+  sha256 "b83ef565342eccd0da1052519aec29335a2af402bfc3a000400ff580bcd28b3b"
 
   def install
     bin.install "libexec/monster.sh" => "monster"
